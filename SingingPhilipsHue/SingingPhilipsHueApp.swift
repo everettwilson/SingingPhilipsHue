a2a2f7a3e695c4a98e2ff9ed7433ecf12d7c697e
Everett Wilson <everettwilson21@me.com>
2024-04-25 21:32:08 -0500

    Initial Commit

---
+++ SingingPhilipsHue/SingingPhilipsHueApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingingPhilipsHueApp.swift
+//  SingingPhilipsHue
+//
+//  Created by Everett Wilson on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SingingPhilipsHueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
